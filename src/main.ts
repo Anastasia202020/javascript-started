b667f1fe400a83ed11f2668a388bd4232b2c9017
Anastasia Murzaeva <a.aleks94@mail.ru>
2023-07-17 21:13:41 +0300

    innit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,47 @@
 import './style.css'
+// задача 1
+// let input = parseFloat(prompt('Введите число','0') as string)
+// let inputT = parseFloat(prompt('Введите степень','0') as string)
+
+// alert(input*input)
+
+// Задача 2
+// let input = parseFloat(prompt('Введите число','0') as string)
+// let inputT = parseFloat(prompt('Введите второе число','0') as string)
+
+// alert((input+inputT)/2)
+
+// Задача 3
+// let input = parseFloat(prompt('Введите сторону квадрата','0') as string)
+
+// alert(input*2)
+
+// Задача 4
+// let input = parseFloat(prompt('Введите колличество киллометров','0') as string)
+// const mili = 0.621371
+
+// alert(input*mili)
+// Задача 5
+// let input = parseFloat(prompt('Введите число 1','0') as string)
+// let inputT = parseFloat(prompt('Введите число 2','0') as string)
+
+// alert(`сумма ${input+inputT}
+// вычитание ${input-inputT}
+// умножение ${input*inputT}
+// деление ${input/inputT}`)
+
+// Задача 6
+// let a = parseFloat(prompt('Введите число ','0') as string)
+// let b = parseFloat(prompt('Введите число ','0') as string)
+
+// alert(-b/a)
+
+// Задача 7
+// const tipH = 24
+// const tipM = 60
+// let hours = parseFloat(prompt('Введите текущий час ','0') as string)
+// let minutes = parseFloat(prompt('Введите текущие минуты ','0') as string)
+// let h = parseInt(`${(tipH*60-hours*60-minutes)/60}`)
+// alert(`часов ${h}
+// минут ${tipM-minutes}`)
+
